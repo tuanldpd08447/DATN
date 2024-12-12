8999662db4ef2699fa4988da3e480686743ccdda
tuanldpd08447 <tuanldpd08447@fpt.edu.vn>
2024-12-12 13:14:21 +0700

    sua them hang cho

--- DATN_QLTiemChung/DATN_QLTiemChung/obj/Debug/net8.0/DATN_QLTiemChung.AssemblyInfo.cs
+++ DATN_QLTiemChung/DATN_QLTiemChung/obj/Debug/net8.0/DATN_QLTiemChung.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DATN_QLTiemChung")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02e3016af7a376f3f9c91f8910047adf407bc3cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6594a9353cd100c646dfc11375b5e53d6635f9a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DATN_QLTiemChung")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DATN_QLTiemChung")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
